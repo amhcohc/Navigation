5150109944a3094ec549c6d79c0ac5fe8c0c0f43
Labong <chochma@coupang.com>
2018-01-13 13:31:25 +0900

    finish navigation controller

--- Navigation/EditViewController.swift
+++ Navigation/EditViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool)
+}
+
 class EditViewController: UIViewController {
     var textWayValue: String = ""
+    var textMessage: String = ""
+    var isOn = false
+    var isZoom = false
+    var delegate: EditDelegate?
+    
+    @IBOutlet var isZoomButton: UIButton!
     @IBOutlet var labelWay: UILabel!
+    @IBOutlet var textMessageField: UITextField!
+    @IBOutlet var switchIsOn: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         labelWay.text = textWayValue
+        textMessageField.text = textMessage
+        switchIsOn.isOn = isOn
+        
+        if isZoom {
+            isZoomButton.setTitle("확대", for: UIControlState.normal)
+        } else {
+            isZoomButton.setTitle("축소", for: UIControlState.normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +57,30 @@ class EditViewController: UIViewController {
     }
     */
     @IBAction func done(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: textMessageField.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+            delegate?.didImageZoomDone(self, isZoom: isZoom)
+        }
+        
         _ = navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func zoomImage(_ sender: UIButton) {
+        if sender.currentTitle == "확대" {
+            isZoom = true
+            isZoomButton.setTitle("축소", for: UIControlState.normal)
+        } else {
+            isZoom = false
+            isZoomButton.setTitle("확대", for: UIControlState.normal)
+        }
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
 }
--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, EditDelegate {
+    let imageOn = UIImage(named: "01.png")
+    let imageOff = UIImage(named: "02.png")
+    
+    var isOn = true
+    var isZoom = true
+    
+    @IBOutlet var textMessageField: UITextField!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = imageOn
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +36,42 @@ class ViewController: UIViewController {
         } else if segue.identifier == "editBarButton" {
             editViewController.textWayValue = "segue : use Bar Button"
         }
+        
+        editViewController.textMessage = textMessageField.text!
+        editViewController.isOn = isOn
+        editViewController.isZoom = isZoom
+        editViewController.delegate = self
     }
 
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        textMessageField.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imageView.image = imageOn
+            self.isOn = true
+        } else {
+            imageView.image = imageOff
+            self.isOn = false
+        }
+    }
+    
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if isZoom {
+            newWidth = imageView.frame.width * scale
+            newHeight = imageView.frame.height * scale
+            imageView.frame.size = CGSize(width: newWidth, height: newHeight)
+            self.isZoom = false
+        } else {
+            newWidth = imageView.frame.width / scale
+            newHeight = imageView.frame.height / scale
+            imageView.frame.size = CGSize(width: newWidth, height: newHeight)
+            self.isZoom = true
+        }
+    }
 }
 
